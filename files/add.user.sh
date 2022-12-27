926e6159cb41cbe6feae78bf53438cf5a7c23301
Plexus Network Academy <turkey.plexus.system@gmail.com>
2022-12-27 20:09:27 +0300

    :wrench: updated :wrench:

--- files/add.user.sh
+++ files/add.user.sh
@@ -1,9 +1,33 @@
-# add encodings  (known_face_encodings)
-# read user name from a  file
-add_name="$(cat user/new_user_name.txt)_face_encoding,\n    abra_face_encoding"
+# read user name from file
+user_name="$(cat user/new_user_name.txt)"
 
-user_encod=$(cat "files/known_face_encodings.py" | sed -r "s#abra_face_encoding#$add_name#g");
+# add encode (known_face_encodings.py)
+add_user_encode="${user_name// /_}_face_encoding,\n    abra_face_encoding"
+user_encod=$(cat "files/known_face_encodings.py" | sed -r "s#abra_face_encoding#$add_user_encode#g");
 
-echo -e "$user_encod" > files/known_face_encodings.py
 
-# add names (known_face_names)
+
+# add name (known_face_names.py)
+add_user_name=$(cat "files/known_face_names.py" | sed -r "s#\"Abra AURORA\"#\"$user_name\",\n    \"Abra AURORA\"#g")
+
+
+echo -e "Choose an image"
+image_dir="$(zenity --file-selection --file-filter='Image files (jpg,png,jpeg) | *.jpg *.jpeg *.png' --file-filter='All files | *')"
+echo -e "image dir: $image_dir"
+
+# add user (users.py)
+change="${user_name// /_}_image = face_recognition.load_image_file(\"$image_dir\")
+${user_name// /_}_face_encoding = face_recognition.face_encodings(${user_name// /_}_image)[0]"
+
+if [[ -f $image_dir ]]; then
+
+    echo -e "$user_encod" > files/known_face_encodings.py
+    echo -e "$add_user_name" > files/known_face_names.py
+    echo -e "\n$change" >> files/users.py
+    echo -e "$user_name" >> files/users.txt
+
+:
+else
+    echo -e "User add proc is aborted"
+:
+fi
--- run.sh
+++ run.sh
@@ -1,27 +1,31 @@
 
 while [[ $choose != "exit" ]]; do
 echo "-------------------------------"
-echo -e "Select module:\n[1] Run script\n[2] Add user\n[3] To do list"
+echo -e "Select module:\n[1] Run script\n[2] Add user\n[3] List users\n[4] To do list"
         read -e -p "$(echo -ne "Choose: ")" choose
 
         echo "$choose"
 
         case $choose in
-           '1' )
+            '1' )
                 bash face_recognition.sh
                 ;;
             '2' )
                 read -e -p "$(echo -ne "Type the new user name: ")" new_user_name
                 echo -e "$new_user_name" > user/new_user_name.txt
-                bash files/add.user_encodings.sh
+                bash files/add.user.sh
                 ;;
-              '3'|'exit' )
+            '3' )
+                cat files/users.txt
+                echo ""
+                ;;
+            '4'|'exit' )
                 clear
                 echo "To Do"
                 cat to\ do
                 ;;
             * )
-                echo -e "Choose options: 1/2"
+                echo -e "Choose options: 1/2/3/4"
             ;;
         esac
 done
